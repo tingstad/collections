c2d0bcb4414b25842f2aa683995671999c429097
U-R2\richard <richard.tingstad@gmail.com>
2015-11-07 22:05:41 +0100

    Test cases for anonymous Condition and Matcher

--- src/test/java/com/rictin/test/condition/ConditionTest.java
+++ src/test/java/com/rictin/test/condition/ConditionTest.java
@@ -10,22 +10,30 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.rictin.test.Person;
 import com.rictin.util.Pipe;
 import com.rictin.util.pipe.Condition;
+import com.rictin.util.pipe.matcher.Matcher;
 import com.rictin.util.pipe.matcher.Num;
 import com.rictin.util.pipe.matcher.Str;
 
 public class ConditionTest {
 
-	@Test
-	public void testAnd() {
-		List<Person> list = Arrays.asList(
+	private List<Person> list;
+	
+	@Before
+	public void setUp() {
+		list = Arrays.asList(
 				new Person("RICHARD", 30),
 				new Person("KIRSTI", 31),
 				new Person("TORSTEIN", 2));
+	}
+	
+	@Test
+	public void testAnd() {
 		Pipe<Person> pipe = Pipe.from(list);
 
 		List<Person> adults = pipe
@@ -40,11 +48,6 @@ public class ConditionTest {
 
 	@Test
 	public void testNumericOnlyCondition() {
-		List<Person> list = Arrays.asList(
-				new Person("RICHARD", 30),
-				new Person("KIRSTI", 31),
-				new Person("TORSTEIN", 2));
-
 		List<Person> persons = Pipe.from(list)
 				.select( Condition.where(4).is(Num.greaterThan(3)) )
 				.toList();
@@ -54,11 +57,6 @@ public class ConditionTest {
 
 	@Test
 	public void testFalseNumericOnlyCondition() {
-		List<Person> list = Arrays.asList(
-				new Person("RICHARD", 30),
-				new Person("KIRSTI", 31),
-				new Person("TORSTEIN", 2));
-
 		List<Person> persons = Pipe.from(list)
 				.select( Condition.where(2).is(Num.greaterThan(3)) )
 				.toList();
@@ -68,10 +66,6 @@ public class ConditionTest {
 
 	@Test
 	public void testManyConditions() {
-		List<Person> list = Arrays.asList(
-				new Person("RICHARD", 30),
-				new Person("KIRSTI", 31),
-				new Person("TORSTEIN", 2));
 		Pipe<Person> pipe = Pipe.from(list);
 
 		List<Person> persons = pipe.select(
@@ -87,11 +81,6 @@ public class ConditionTest {
 
 	@Test
 	public void testStringCondition() {
-		List<Person> list = Arrays.asList(
-				new Person("RICHARD", 30),
-				new Person("KIRSTI", 31),
-				new Person("TORSTEIN", 2));
-
 		List<Person> persons = Pipe.from(list).select(
 				Condition.where( Pipe.item(list).getName() ).is(Str.startsWith("R"))
 				).toList();
@@ -102,17 +91,41 @@ public class ConditionTest {
 
 	@Test
 	public void testStringConditionWithoutMatcher() {
-		List<Person> list = Arrays.asList(
-				new Person("RICHARD", 30),
-				new Person("KIRSTI", 31),
-				new Person("TORSTEIN", 2));
-
 		List<Person> persons = Pipe.from(list).select(
 				Condition.where( Pipe.item(list).getName() ).startsWith("R")
 				).toList();
 
 		Assert.assertEquals(1, persons.size());
 		Assert.assertEquals("RICHARD", persons.get(0).getName());
+	}
+
+	@Test
+	public void testAnonymousCondition() {
+		List<Person> persons = Pipe.from(list).select(
+				new Condition<Person>() {
+
+					public boolean satisfies(Person element) {
+						return Integer.valueOf(31).equals(element.getAge());
+					}
+				}).toList();
+
+		Assert.assertEquals(1, persons.size());
+		Assert.assertEquals("KIRSTI", persons.get(0).getName());
+	}
+
+	@Test
+	public void testAnonymousMatcher() {
+		List<Person> persons = Pipe.from(list).select(
+				Condition.where( Pipe.item(list).getAge() ).is(new Matcher<Integer>(1) {
+					
+					// Age is 10*X + _1_
+					public boolean matches(Integer value, Integer input) {
+						return input % 10 == value;
+					}
+				})).toList();
+
+		Assert.assertEquals(1, persons.size());
+		Assert.assertEquals("KIRSTI", persons.get(0).getName());
 	}
 
 }
